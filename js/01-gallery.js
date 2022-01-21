33f52d0a808ad6d6309d905bddf091808a80b603
StanislavProtashchuk <stasprotaskhyk@gmail.com>
2022-01-21 21:23:01 +0200

    gomeworkReady

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+galleryContainer.addEventListener('click', onClickEvent);
+
+// console.log(createGallery(galleryItems));
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>`;
+    }).join('');    
+}
+
+function onClickEvent(e) {
+    e.preventDefault();
+    
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+    const imageLink = e.target.dataset.source;
+    const imageAlt = e.target.dataset.alt;
+    const imageToShow = `<img src="${imageLink}", alt="${imageAlt}">`;
+
+    console.log(imageLink);
+
+    const instance = basicLightbox.create(imageToShow);
+
+    instance.show();
+
+    window.addEventListener('keydown', escapeClick);
+
+    function escapeClick(e) {
+        if (e.key === 'Escape') {
+            instance.close();
+            window.removeEventListener('keydown', escapeClick);
+        }
+        console.log(e.key);
+    }
+}
+    
+
